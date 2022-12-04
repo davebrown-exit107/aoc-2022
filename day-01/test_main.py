a603d117be429667f9ffda6f69765cfbceb99ca7
Dave Brown <dave.brown@exit107.com>
2022-12-04 10:08:51 -0700

    Fix typo with test assert

--- day-01/test_main.py
+++ day-01/test_main.py
@@ -14,7 +14,7 @@ class TestCalories(unittest.TestCase):
     def test_top_three_calories(self):
         sample_input = [ "1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000"]
 
-        want = 24000
+        want = 45000
         got = top_three_calories(sample_input)
 
         self.assertEqual(got, want)
