9a237524235e38573a622372650437f2b9f35441
Dave Brown <dave.brown@exit107.com>
2022-12-04 09:24:02 -0700

    Day 1 part 2 complete
    Really embarassing because I just wasn't printing out the correct output
    and so my tests would pass but my final results wouldn't. Meh, onward
    and upward!

--- day-01/main.go
+++ day-01/main.go
@@ -4,35 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
-func CalorieCalculator(calorie_list []string) (int, int) {
-	cur_elf := 1
-	cur_elf_cals := 0
-	top_elf := 0
-	top_elf_cals := 0
-	for _, cals := range calorie_list {
+func TopThreeCalories(calorieList []string) int {
+	totalElves := 0
+	curCals := 0
+	var totalCals []int
+	for _, cals := range calorieList {
 		if cals == "" {
-			// Output elf status
-			fmt.Printf("%+v: %+v\n", cur_elf, cur_elf_cals)
-			// Check for top status
-			if cur_elf_cals > top_elf_cals {
-				top_elf_cals = cur_elf_cals
-				top_elf = cur_elf
-			}
-			// Next elf up
-			cur_elf++
-			cur_elf_cals = 0
+			totalCals = append(totalCals, curCals)
+			totalElves++
+			curCals = 0
 		} else {
 			int_cals, err := strconv.Atoi(cals)
 			if err != nil {
 				panic(err)
 			}
-			cur_elf_cals += int_cals
+			curCals += int_cals
 		}
 	}
-	return top_elf, top_elf_cals
+	totalCals = append(totalCals, curCals)
+	sort.Ints(totalCals)
+	return totalCals[totalElves] + totalCals[totalElves-1] + totalCals[totalElves-2]
+}
+
+func TopCalories(calorieList []string) int {
+	curCals := 0
+	topCals := 0
+	for _, cals := range calorieList {
+		if cals == "" {
+			// Check for top status
+			if curCals > topCals {
+				topCals = curCals
+			}
+			// Next elf up
+			curCals = 0
+		} else {
+			int_cals, err := strconv.Atoi(cals)
+			if err != nil {
+				panic(err)
+			}
+			curCals += int_cals
+		}
+	}
+	if curCals > topCals {
+		topCals = curCals
+	}
+	return topCals
 }
 
 func main() {
@@ -51,8 +71,10 @@ func main() {
 		fileLines = append(fileLines, fileScanner.Text())
 	}
 
-	topElf, topCals := CalorieCalculator(fileLines)
+	topCals := TopCalories(fileLines)
+	topThreeCals := TopThreeCalories(fileLines)
 
-	fmt.Printf("The top elf (%v) has a calorie count of %v\n", topElf, topCals)
+	fmt.Printf("The top elf has a calorie count of %v\n", topCals)
+	fmt.Printf("The top three elves have a calorie count of %v\n", topThreeCals)
 
 }
--- day-01/main_test.go
+++ day-01/main_test.go
@@ -2,17 +2,26 @@ package main
 
 import "testing"
 
-func TestCalorieCounter(t *testing.T) {
+func TestTopThreeCalories(t *testing.T) {
 	sample_input := []string{"1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000"}
 
-	got_elf, got_cals := CalorieCalculator(sample_input)
-	expect_elf, expect_cals := 4, 24000
+	got := TopThreeCalories(sample_input)
+	expected := 45000
 
-	if got_elf != expect_elf {
-		t.Errorf("expected '%d' but got '%d'", expect_elf, got_elf)
+	if got != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, got)
 	}
-	if got_cals != expect_cals {
-		t.Errorf("expected '%d' but got '%d'", expect_cals, got_cals)
+
+}
+
+func TestTopCalories(t *testing.T) {
+	sample_input := []string{"1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000"}
+
+	got := TopCalories(sample_input)
+	expected := 24000
+
+	if got != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, got)
 	}
 
 }
