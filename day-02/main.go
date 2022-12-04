afd3fb6bebaa923602afc967eb00e727b4e73de8
Dave Brown <dave.brown@exit107.com>
2022-12-04 12:40:03 -0700

    Day 2 pt 1 complete - golang
    Really had a hard time with the score outcome if logic. Ultimately I
    think I should encode it in a matrix and access it via dual idx, but
    that's for refactoring!

--- day-02/main.go
+++ day-02/main.go
@@ -4,10 +4,71 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+func ScoreShape(shape string) int {
+	if shape == "X" {
+		return 1
+	}
+	if shape == "Y" {
+		return 2
+	}
+	if shape == "Z" {
+		return 3
+	}
+	panic("didn't recognize the shape")
+}
+
+func ScoreOutcome(shapes []string) int {
+	them := shapes[0]
+	me := shapes[1]
+	if them == "A" {
+		if me == "X" {
+			return 3
+		}
+		if me == "Y" {
+			return 6
+		}
+		if me == "Z" {
+			return 0
+		}
+	}
+	if them == "B" {
+		if me == "X" {
+			return 0
+		}
+		if me == "Y" {
+			return 3
+		}
+		if me == "Z" {
+			return 6
+		}
+	}
+	if them == "C" {
+		if me == "X" {
+			return 6
+		}
+		if me == "Y" {
+			return 0
+		}
+		if me == "Z" {
+			return 3
+		}
+	}
+	panic("couldn't recognize one shape or another")
+}
+
 func ScoreRounds(rounds []string) int {
-	return 0
+	totalScore := 0
+	for _, moves := range rounds {
+		moveArr := strings.Split(moves, " ")
+		shapeScore := ScoreShape(moveArr[1])
+		outcomeScore := ScoreOutcome(moveArr)
+		totalScore += shapeScore + outcomeScore
+		fmt.Println(shapeScore, outcomeScore)
+	}
+	return totalScore
 }
 
 func main() {
@@ -28,6 +89,6 @@ func main() {
 
 	totalScore := ScoreRounds(fileLines)
 
-	fmt.Printf("The total score is %v", totalScore)
+	fmt.Printf("The total score is %v\n", totalScore)
 
 }
