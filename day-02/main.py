652ed2beee3258279273297c06f315c7a1b73011
Dave Brown <dave.brown@exit107.com>
2022-12-04 13:24:16 -0700

    Day 2 pt 1+2 done - python
    Kind of testing out the matrix idea. It's already cleaner, I just need a
    cleaner way of indexing the moves so there are fewer `if` structures as
    it's not super easy to debug.

--- day-02/main.py
+++ day-02/main.py
@@ -1,11 +1,63 @@
 import os
 
 def score_game(input_lines):
-    return 0
+    outcome_matrix = [[3,6,0],[0,3,6],[6,0,3]]
+    move_matrix = [1,2,3]
+    total_score = 0
+    for game in input_lines:
+        them, us = game.split(" ")
+        if them == "A":
+            if us == 'X':
+                total_score += outcome_matrix[0][0] + move_matrix[0]
+            if us == 'Y':
+                total_score += outcome_matrix[0][1] + move_matrix[1]
+            if us == 'Z':
+                total_score += outcome_matrix[0][2] + move_matrix[2]
+        if them == "B":
+            if us == 'X':
+                total_score += outcome_matrix[1][0] + move_matrix[0]
+            if us == 'Y':
+                total_score += outcome_matrix[1][1] + move_matrix[1]
+            if us == 'Z':
+                total_score += outcome_matrix[1][2] + move_matrix[2]
+        if them == "C":
+            if us == 'X':
+                total_score += outcome_matrix[2][0] + move_matrix[0]
+            if us == 'Y':
+                total_score += outcome_matrix[2][1] + move_matrix[1]
+            if us == 'Z':
+                total_score += outcome_matrix[2][2] + move_matrix[2]
+    return total_score
 
 
 def new_score_game(input_lines):
-    return 0
+    move_matrix = [[3,1,2],[1,2,3],[2,3,1]]
+    outcome_matrix = [0,3,6]
+    total_score = 0
+    for game in input_lines:
+        them, us = game.split(" ")
+        if them == "A":
+            if us == 'X':
+                total_score += move_matrix[0][0] + outcome_matrix[0]
+            if us == 'Y':
+                total_score += move_matrix[0][1] + outcome_matrix[1]
+            if us == 'Z':
+                total_score += move_matrix[0][2] + outcome_matrix[2]
+        if them == "B":
+            if us == 'X':
+                total_score += move_matrix[1][0] + outcome_matrix[0]
+            if us == 'Y':
+                total_score += move_matrix[1][1] + outcome_matrix[1]
+            if us == 'Z':
+                total_score += move_matrix[1][2] + outcome_matrix[2]
+        if them == "C":
+            if us == 'X':
+                total_score += move_matrix[2][0] + outcome_matrix[0]
+            if us == 'Y':
+                total_score += move_matrix[2][1] + outcome_matrix[1]
+            if us == 'Z':
+                total_score += move_matrix[2][2] + outcome_matrix[2]
+    return total_score
 
 if __name__ == '__main__':
     with open('input.txt') as in_fh:
