b82674b98e8ad24feee12ece529b467333af7ce7
Dave Brown <dave.brown@exit107.com>
2022-12-04 12:59:30 -0700

    Day 2 pt 2 complete - golang
    Same thoughts as part 1, I'd like to refactor this using a matrix lookup
    as I think it's a bit more elegant.

--- day-02/main.go
+++ day-02/main.go
@@ -59,6 +59,69 @@ func ScoreOutcome(shapes []string) int {
 	panic("couldn't recognize one shape or another")
 }
 
+func NewScoreOutcome(outcome string) int {
+	if outcome == "X" {
+		return 0
+	}
+	if outcome == "Y" {
+		return 3
+	}
+	if outcome == "Z" {
+		return 6
+	}
+	panic("didn't recognize the outcome string")
+}
+
+func NewScoreShape(shapes []string) int {
+	them := shapes[0]
+	outcome := shapes[1]
+	if them == "A" {
+		if outcome == "X" {
+			return 3
+		}
+		if outcome == "Y" {
+			return 1
+		}
+		if outcome == "Z" {
+			return 2
+		}
+	}
+	if them == "B" {
+		if outcome == "X" {
+			return 1
+		}
+		if outcome == "Y" {
+			return 2
+		}
+		if outcome == "Z" {
+			return 3
+		}
+	}
+	if them == "C" {
+		if outcome == "X" {
+			return 2
+		}
+		if outcome == "Y" {
+			return 3
+		}
+		if outcome == "Z" {
+			return 1
+		}
+	}
+	panic("didn't recognize either the shape or the outcome")
+}
+
+func NewScoreRounds(rounds []string) int {
+	totalScore := 0
+	for _, moves := range rounds {
+		moveArr := strings.Split(moves, " ")
+		outcomeScore := NewScoreOutcome(moveArr[1])
+		shapeScore := NewScoreShape(moveArr)
+		totalScore += shapeScore + outcomeScore
+	}
+	return totalScore
+}
+
 func ScoreRounds(rounds []string) int {
 	totalScore := 0
 	for _, moves := range rounds {
@@ -66,7 +129,6 @@ func ScoreRounds(rounds []string) int {
 		shapeScore := ScoreShape(moveArr[1])
 		outcomeScore := ScoreOutcome(moveArr)
 		totalScore += shapeScore + outcomeScore
-		fmt.Println(shapeScore, outcomeScore)
 	}
 	return totalScore
 }
@@ -88,7 +150,9 @@ func main() {
 	}
 
 	totalScore := ScoreRounds(fileLines)
-
 	fmt.Printf("The total score is %v\n", totalScore)
+
+	newTotalScore := NewScoreRounds(fileLines)
+	fmt.Printf("The new total score is %v\n", newTotalScore)
 
 }
--- day-02/main_test.go
+++ day-02/main_test.go
@@ -12,3 +12,14 @@ func TestScoreRounds(t *testing.T) {
 		t.Errorf("expected %d, got %d.", want, got)
 	}
 }
+
+func TestNewScoreRounds(t *testing.T) {
+	sample_input := []string{"A Y", "B X", "C Z"}
+
+	want := 12
+	got := NewScoreRounds(sample_input)
+
+	if want != got {
+		t.Errorf("expected %d, got %d.", want, got)
+	}
+}
