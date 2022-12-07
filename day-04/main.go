eeba95fa2b7600e34ce0f0be74b734dd10b800c8
Dave Brown <dave.brown@exit107.com>
2022-12-06 20:46:21 -0700

    Day 4 pt 2 complete - golang
    I really struggled with the logic of implementing the non-overlapping
    lookup. I think I should have been drawing it out rather than just
    picturing it in my head. This'll be fun to refactor later.

--- day-04/main.go
+++ day-04/main.go
@@ -28,13 +28,16 @@ func isOverlapping(groupA, groupB string) bool {
 	if err != nil {
 		os.Exit(1)
 	}
-	if (groupAStart <= groupBStart &&
-		groupAEnd >= groupBEnd) ||
-		(groupBStart <= groupAStart &&
-			groupBEnd >= groupAEnd) {
-		return true
-	} else {
+	// instead of before, we're detecting if they don't overlap and flipping the return
+	// group a is before group b
+	if groupAStart < groupBStart &&
+		groupAEnd < groupBStart ||
+		// group b is before group a
+		groupBStart < groupAStart &&
+			groupBEnd < groupAStart {
 		return false
+	} else {
+		return true
 	}
 }
 
@@ -45,6 +48,49 @@ func CountOverlappingAssignments(assignments []string) int {
 		//evaluate the groups to look for overlapping assignments
 		curGroup = strings.Split(assignment, ",")
 		if isOverlapping(curGroup[0], curGroup[1]) == true {
+			overlaps++
+		}
+		curGroup = make([]string, 0)
+	}
+	return overlaps
+}
+func isFullyOverlapping(groupA, groupB string) bool {
+	// parse out the ranges, there's got to be a better way to do this
+	groupAPairs := strings.Split(groupA, "-")
+	groupBPairs := strings.Split(groupB, "-")
+	groupAStart, err := strconv.Atoi(groupAPairs[0])
+	if err != nil {
+		os.Exit(1)
+	}
+	groupAEnd, err := strconv.Atoi(groupAPairs[1])
+	if err != nil {
+		os.Exit(1)
+	}
+	groupBStart, err := strconv.Atoi(groupBPairs[0])
+	if err != nil {
+		os.Exit(1)
+	}
+	groupBEnd, err := strconv.Atoi(groupBPairs[1])
+	if err != nil {
+		os.Exit(1)
+	}
+	if (groupAStart <= groupBStart &&
+		groupAEnd >= groupBEnd) ||
+		(groupBStart <= groupAStart &&
+			groupBEnd >= groupAEnd) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func CountFullyOverlappingAssignments(assignments []string) int {
+	curGroup := make([]string, 0)
+	overlaps := 0
+	for _, assignment := range assignments {
+		//evaluate the groups to look for overlapping assignments
+		curGroup = strings.Split(assignment, ",")
+		if isFullyOverlapping(curGroup[0], curGroup[1]) == true {
 			overlaps++
 		}
 		curGroup = make([]string, 0)
@@ -69,7 +115,9 @@ func main() {
 	}
 
 	// AoC work starts here
+	fullyOverlaps := CountFullyOverlappingAssignments(fileLines)
+	fmt.Printf("Count of full overlaps: %v\n", fullyOverlaps)
+
 	overlaps := CountOverlappingAssignments(fileLines)
 	fmt.Printf("Count of overlaps: %v\n", overlaps)
-
 }
--- day-04/main_test.go
+++ day-04/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestCountOverlappingAssignments(t *testing.T) {
+func TestCountFullyOverlappingAssignments(t *testing.T) {
 	sample_input := []string{
 		"2-4,6-8",
 		"2-3,4-5",
@@ -12,26 +12,26 @@ func TestCountOverlappingAssignments(t *testing.T) {
 		"2-6,4-8"}
 
 	want := 2
-	got := CountOverlappingAssignments(sample_input)
+	got := CountFullyOverlappingAssignments(sample_input)
 
 	if want != got {
 		t.Errorf("expected %d, got %d.", want, got)
 	}
 }
 
-//func TestNewScoreRounds(t *testing.T) {
-//	sample_input := []string{
-//		"2-4,6-8",
-//		"2-3,4-5",
-//		"5-7,7-9",
-//		"2-8,3-7",
-//		"6-6,4-6",
-//		"2-6,4-8"}
-//
-//	want := 12
-//	got := NewScoreRounds(sample_input)
-//
-//	if want != got {
-//		t.Errorf("expected %d, got %d.", want, got)
-//	}
-//}
+func TestCountOverlappingAssignments(t *testing.T) {
+	sample_input := []string{
+		"2-4,6-8",
+		"2-3,4-5",
+		"5-7,7-9",
+		"2-8,3-7",
+		"6-6,4-6",
+		"2-6,4-8"}
+
+	want := 4
+	got := CountOverlappingAssignments(sample_input)
+
+	if want != got {
+		t.Errorf("expected %d, got %d.", want, got)
+	}
+}
