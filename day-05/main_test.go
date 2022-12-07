b497e4ab3f95e19ac9f0dcf2f1274779543740a8
Dave Brown <dave.brown@exit107.com>
2022-12-06 20:59:28 -0700

    Add test data for pt 1

--- day-05/main_test.go
+++ day-05/main_test.go
@@ -4,12 +4,15 @@ import "testing"
 
 func TestCountFullyOverlappingAssignments(t *testing.T) {
 	sample_input := []string{
-		"2-4,6-8",
-		"2-3,4-5",
-		"5-7,7-9",
-		"2-8,3-7",
-		"6-6,4-6",
-		"2-6,4-8"}
+		"	   [D]    ",
+		"[N] [C]    ",
+		"[Z] [M] [P]",
+		" 1   2   3 ",
+		"",
+		"move 1 from 2 to 1",
+		"move 3 from 1 to 3",
+		"move 2 from 2 to 1",
+		"move 1 from 1 to 2"}
 
 	want := 2
 	got := CountFullyOverlappingAssignments(sample_input)
